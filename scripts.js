9125fb1e0545d8a088d560e5bf34c7503e474f44
Sam Kirsch and Sandro Mijatovic <sam@>
2017-01-09 15:39:53 -0800

    add the capFunction, capReverseFunction, and a combination of the two

--- scripts.js
+++ scripts.js
@@ -1,9 +1,34 @@
-var sentence = "test test test test";
-var first = sentence.charAt(0).toUpperCase();
-var last = sentence.charAt(sentence.length-1).toUpperCase();
+// var sentence = "the rains in spain are berliners";
+// var first = sentence.charAt(0).toUpperCase();
+// var last = sentence.charAt(sentence.length-1).toUpperCase();
+//
+// var comboForwards = first + last;
+// var comboBackwards = last + first;
+//
+// alert("First function results: " + comboForwards + " ///  Second function results " + comboBackwards);
+//
+// var sentenceWithout = sentence.substring(1,sentence.length-1);
+// var newSentence = first + sentenceWithout + last;
+// alert(newSentence);
+
+var capFirst = function () {
+  var first = prompt("Enter a sentence: ");
+  var firstL = first.charAt(0).toUpperCase();
+  var lastL = first.charAt(first.length-1).toUpperCase();
+  var outPut = firstL + lastL;
+  alert(outPut);
+  return outPut;
+}
 
 
-alert(last);
+var capReverse = function(capFirst) {
+  var capRev = capFirst.charAt(1) + capFirst.charAt(0);
+  alert(capRev);
+  return capRev;
+}
 
-var testChange = first + "  " + last;
-alert(testChange);
+var thirdFunc = function() {
+  capReverse(capFirst());
+}
+
+thirdFunc();
