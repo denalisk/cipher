7da3fa4cc007c82f87c751f5e5bb70ed48a83d83
Sam Kirsch and Sandro Mijatovic <sandro@>
2017-01-09 15:16:12 -0800

    will allow us to change the first and last character to uppercase

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var sentence = "test test test test";
+var first = sentence.charAt(0).toUpperCase();
+var last = sentence.charAt(sentence.length-1).toUpperCase();
+
+
+alert(last);
+
+var testChange = first + "  " + last;
+alert(testChange);
