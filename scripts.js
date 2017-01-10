aa3c5dcc01f29be27ca23fb3857ae31e3485e7e9
Sam Kirsch and Sandro Mijatovic <sam@>
2017-01-09 16:21:47 -0800

    fix the fourth function and make the program work through function four

--- scripts.js
+++ scripts.js
@@ -9,32 +9,32 @@ var capFirst = function(sentence) {
   var firstL = sentence.charAt(0).toUpperCase();
   var lastL = sentence.charAt(sentence.length-1).toUpperCase();
   var outPut = firstL + lastL;
-  alert(outPut);
+  // alert(outPut);
   return outPut;
 }
 
 var capReverse = function(capFirst) {
   var capRev = capFirst.charAt(1) + capFirst.charAt(0);
-  alert(capRev);
+  // alert(capRev);
   return capRev;
 }
 
 var thirdFunc = function(sentence) {
   var capRev = capReverse(capFirst(sentence));
   var together = sentence + capRev;
-  alert(together);
+  // alert(together);
   return together;
 }
 
-// var finalVar = thirdFunc(sentence);
 // thirdFunc(sentence);
-
+// var half = sentence.length / 2;
+// alert(charAt(hald))
+// alert(half);
 
 var fourthFunc = function(sentence) {
   var half = sentence.length / 2;
   var finalVar = thirdFunc(sentence);
-  var mid = charAt(half);
-  alert(mid);
+  var mid = sentence.charAt(half);
   var final = mid + finalVar;
   alert(final);
 }
