411227d5bd06d5cff3e2d8d79ec2f3adce334eb8
Sam Kirsch and Sandro Mijatovic <sam@>
2017-01-09 16:16:29 -0800

    add additional functions and debugging steps

--- scripts.js
+++ scripts.js
@@ -1,25 +1,17 @@
-// var sentence = "the rains in spain are berliners";
-// var first = sentence.charAt(0).toUpperCase();
-// var last = sentence.charAt(sentence.length-1).toUpperCase();
-//
-// var comboForwards = first + last;
-// var comboBackwards = last + first;
-//
-// alert("First function results: " + comboForwards + " ///  Second function results " + comboBackwards);
-//
-// var sentenceWithout = sentence.substring(1,sentence.length-1);
-// var newSentence = first + sentenceWithout + last;
-// alert(newSentence);
+var getSentence = function() {
+  var sentence = prompt("Enter a sentence: ");
+  return sentence;
+}
 
-var capFirst = function () {
-  var first = prompt("Enter a sentence: ");
-  var firstL = first.charAt(0).toUpperCase();
-  var lastL = first.charAt(first.length-1).toUpperCase();
+var sentence = getSentence();
+
+var capFirst = function(sentence) {
+  var firstL = sentence.charAt(0).toUpperCase();
+  var lastL = sentence.charAt(sentence.length-1).toUpperCase();
   var outPut = firstL + lastL;
   alert(outPut);
   return outPut;
 }
-
 
 var capReverse = function(capFirst) {
   var capRev = capFirst.charAt(1) + capFirst.charAt(0);
@@ -27,8 +19,24 @@ var capReverse = function(capFirst) {
   return capRev;
 }
 
-var thirdFunc = function() {
-  capReverse(capFirst());
+var thirdFunc = function(sentence) {
+  var capRev = capReverse(capFirst(sentence));
+  var together = sentence + capRev;
+  alert(together);
+  return together;
 }
 
-thirdFunc();
+// var finalVar = thirdFunc(sentence);
+// thirdFunc(sentence);
+
+
+var fourthFunc = function(sentence) {
+  var half = sentence.length / 2;
+  var finalVar = thirdFunc(sentence);
+  var mid = charAt(half);
+  alert(mid);
+  var final = mid + finalVar;
+  alert(final);
+}
+
+fourthFunc(sentence);
